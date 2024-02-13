1195f17c82107b5a0cc760106810a18949534837
eslam <elbadry693@gmail.com>
2024-02-14 00:41:20 +0200

    add therapist schedule crud

--- app/Http/Controllers/PhoneVerifyController.php
+++ app/Http/Controllers/PhoneVerifyController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\PhoneVerifyRequest;
 use App\Mail\ResetPasswordMail;
 use App\Models\PasswordResetCode;
