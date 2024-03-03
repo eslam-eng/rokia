bba73f6209de8ff62206ae5f7d1759f215c9b748
eslam <elbadry693@gmail.com>
2024-03-04 01:00:44 +0200

    add models to morph map in service provider

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('send-reminders')->dailyAt('9:00');
+        $schedule->command('send-reminders')->daily();
         $schedule->command('queue:work --stop-when-empty')->everyMinute();
         $schedule->command('queue:restart')->everyFiveMinutes();
     }
