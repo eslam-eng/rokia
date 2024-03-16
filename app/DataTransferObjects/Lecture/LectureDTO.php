2e8bab98a94193b302225a7827107f34fe7b9b87
eslam <elbadry693@gmail.com>
2024-03-16 22:53:19 +0200

    fix lecture validation

--- app/DataTransferObjects/Lecture/LectureDTO.php
+++ app/DataTransferObjects/Lecture/LectureDTO.php
@@ -86,7 +86,7 @@ class LectureDTO extends BaseDTO
             'image_cover' => 'nullable|file|mimes:png,jpg,jpeg',
         ];
         if (request()->url() == route('lectures.store'))
-            $rules['audio_file'] = 'required|audio|duration_min:60|max:25600';
+            $rules['audio_file'] = 'required|file|mimes:mp3,wav,ogg,m4a|max:25600';
         return $rules;
     }
 
