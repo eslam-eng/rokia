889da7e583411e4f1fbd0b274f64ade18240a52e
eslam <elbadry693@gmail.com>
2024-04-06 22:06:57 +0200

    add logger

--- app/Models/Lecture.php
+++ app/Models/Lecture.php
@@ -82,11 +82,8 @@ class Lecture extends Model implements HasMedia
     protected function isAvailable(): Attribute
     {
         $date = Carbon::parse($this->publish_date);
-        dd($date,Carbon::now()->timezone('Asia/Riyadh'),$date->lte(Carbon::now()));
-        if ($date->lte(Carbon::now()))
-            logger('yessssssss');
         return Attribute::make(
-            get: fn() => $date->lte(Carbon::now()),
+            get: fn() => $date->lte(Carbon::now()->timezone('Asia/Riyadh')),
         );
     }
 }
